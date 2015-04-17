7e5a794e6ce91c610d243193ceeef4372f29ff83
Anastas Dancha <anapsix@random.io>
2015-04-17 16:39:11 -0400

    EXAMPLES: better description of script arguments

--- haproxy_discovery.sh
+++ haproxy_discovery.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #
 # Get list of Frontends and Backends from HAPROXY
+# Example: ./haproxy_discovery.sh [/var/run/haproxy/info.sock] FRONTEND|BACKEND|SERVERS
+# First argument is optional and should be used to set location of your HAPROXY socket
+# Second argument is should be either FRONTEND, BACKEND or SERVERS, will default to FRONTEND if not set
 #
-HAPROXY_SOCK=$(echo $1 | grep -Po "^/[^\s]+" || echo "/var/run/haproxy/info.sock")
+# !! Make sure the user running this script has Read/Write permissions to that socket !!
+#
+## haproxy.cfg snippet
+#  global
+#  stats socket /run/haproxy/info.sock  mode 666 level user
 
-#echo "DEBUG: HAPROXY_SOCK: $HAPROXY_SOCK" >&2
+HAPROXY_SOCK=$(echo $1 | grep -Po "^/[^\s]+" || echo "/var/run/haproxy/info.sock")
 
 get_stats() {
 	echo "show stat" | socat ${HAPROXY_SOCK} stdio 2>/dev/null | grep -v "^#"
@@ -26,7 +33,7 @@ case $1 in
 	*) END="FRONTEND" ;;
 esac
 
-for frontend in $(echo "show stat" | socat ${HAPROXY_SOCK} stdio 2>/dev/null | grep -v "#" | grep "$END" | cut -d, -f1 | uniq); do
+for frontend in $(get_stats | grep "$END" | cut -d, -f1 | uniq); do
     felist="$felist,"'{"{#'${END}'_NAME}":"'$frontend'"}'
 done
 echo '{"data":['${felist#,}']}'
